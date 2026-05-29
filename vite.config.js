a896e0c66d15972c31e42f67cb220b106b21b864
JuanFer-NL <juanfernandez.nl@gmail.com>
2026-05-29 03:59:18 -0300

    Corrección menor de errores

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,8 @@ export default defineConfig({
   plugins: [
     react(),
     legacy({
-      // Agregamos 'ie >= 11' para obligarlo a usar el formato ES5 sí o sí
-      targets: ['ie >= 11', 'ios >= 9', 'safari >= 9'], 
+      targets: ['ios >= 9', 'safari >= 9'], 
       polyfills: true
     })
-  ],
-  build: {
-    // Esto asegura que el código principal baje un cambio y no use sintaxis muy nueva
-    target: 'es2015' 
-  }
+  ]
 })
