26bcd70e6f42fff6245b7d7e233ee8bc2bad6ff2
JuanFer-NL <juanfernandez.nl@gmail.com>
2026-05-29 03:53:11 -0300

    Corrección menor de errores

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import legacy from '@vitejs/plugin-legacy'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     legacy({
-      // Podés ajustar la versión de iOS dependiendo de la que tenga tu iPad Mini
-      targets: ['iOS >= 9', 'Safari >= 9'], 
+      // Agregamos 'ie >= 11' para obligarlo a usar el formato ES5 sí o sí
+      targets: ['ie >= 11', 'ios >= 9', 'safari >= 9'], 
       polyfills: true
     })
   ],
+  build: {
+    // Esto asegura que el código principal baje un cambio y no use sintaxis muy nueva
+    target: 'es2015' 
+  }
 })
